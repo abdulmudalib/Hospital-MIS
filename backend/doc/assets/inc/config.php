06413d7cad9e6916087bffa0e62409a5518fa0c2
Abdulmutalib Murshid <your-email@example.com>
2025-07-23 00:59:21 +0300

    Initial commit

---
+++ backend/doc/assets/inc/config.php
@@ -0,0 +1,13 @@
+<?php
+$dbuser = "root";
+$dbpass = ""; // Step 1: Changed from "database-password" to ""
+$host = "localhost";
+$db = "hmisphp"; // Step 2: Changed from "database-name" to "hmisphp"
+
+$mysqli = new mysqli($host, $dbuser, $dbpass, $db);
+
+// Optional: Check connection
+if ($mysqli->connect_error) {
+    die("Connection failed: " . $mysqli->connect_error);
+}
+?>
